15646d436c0babce08ded1a0df268c3d04f284e1
Hibi Miyazaki <hibi.miyazaki@gmail.com>
2025-07-06 09:07:48 +0900

    Add initial implementation of Undefined module with placeholder types

---
+++ src/OrderTaking/Undefined.hs
@@ -0,0 +1,17 @@
+﻿module OrderTaking.Undefined where
+
+import Data.Void (Void)
+
+-- a used for placeholders
+type Undefined = Void
+
+-- Will be replaced with ellipses in the book.
+-- Used in the example code to let the code type check without having to provide full details.
+type DotDotDot = Undefined 
+type PrivateDotDotDot = Undefined 
+dotDotDot = error "undefined"
+
+-- Will be replaced with question marks in the book.
+question = error "to be defined"
+
+notImplemented = error "not implemented"
